7f7d6f662510aacb2074ffbb64994ded4298dd53
Garrett Estrin <garrett.estrin@gmail.com>
2017-05-02 14:05:59 -0700

    get url to pull data for yesterdays games

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
 // Custom JavaScript by Garrett Estrin | GarrettEstrin.com
 
-// Function to find the last game the ducks played
-function findLastGame(){
 
+
+// Function to find the last game the ducks played
+// example api call for all games on specific date
+// http://api.sportradar.us/nhl-ot4/games/2017/05/01/schedule.json?api_key=gpku2at4tqngufd65hyzgfcw
+// http://api.sportradar.us/nhl-ot4/games/117/4/2/schedule.json?api_key=gpku2at4tqngufd65hyzgfcw
+function findLastGame(){
+  var base = 'http://api.sportradar.us/nhl-ot4/games/';
+  var dt = new Date();
+  var year = yyyy + '/';
+  var month = mm +'/';
+  var date = dd + '/';
+  var top = 'schedule.json?api_key=gpku2at4tqngufd65hyzgfcw';
+
+  var url = base + year + month + date + top;
+  console.log(url);
 }
 // Function to pull data from that game
 function getLastGame(){
@@ -20,3 +33,24 @@ function getScores(){
 function compareScores(){
 
 }
+
+// Function and variables to get yesterdays date
+var dd;
+var mm;
+var yyyy;
+function getDate(){
+  today = new Date();
+  yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  dd = yesterday.getDate();
+  mm = yesterday.getMonth()+1; //January is 0!
+  yyyy = yesterday.getFullYear();
+  if(dd<10){
+    dd='0'+dd
+  } if(mm<10){
+    mm='0'+mm
+  }
+
+}
+
+getDate();
