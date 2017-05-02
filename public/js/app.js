eed67f4362c8f849615e6e77bb6204a68be2f2dd
Garrett Estrin <garrett.estrin@gmail.com>
2017-05-02 13:33:06 -0700

    skeleton of logic to get data

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,22 @@
 // Custom JavaScript by Garrett Estrin | GarrettEstrin.com
-var name = "garrett"
+
+// Function to find the last game the ducks played
+function findLastGame(){
+
+}
+// Function to pull data from that game
+function getLastGame(){
+
+}
+// Function to determine if ducks were home team
+function homeTeam(){
+
+}
+// Function to get home and away scores
+function getScores(){
+
+}
+// Function to compare the scores bases on ducks being home team or not
+function compareScores(){
+
+}
